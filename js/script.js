2a82582ed8ee06dccb543e447055d4f9992588d5
Kajetan Blaszczyk <k.blaszczyyk@gmail.com>
2023-06-25 18:38:56 +0200

    adding new task

--- js/script.js
+++ js/script.js
@@ -10,12 +10,19 @@
         },
     ];
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    };
+
     const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
             htmlString += `
-              <li>
+              <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
               ${task.content}
               </li>
             `;
@@ -24,8 +31,24 @@
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    };
+
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
